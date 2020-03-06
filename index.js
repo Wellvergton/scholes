e7c3f93e233343579e9fac0ad2634012c2e30a03
Wellvergton Marques <wmc.jp@outlook.com>
2020-03-06 00:55:42 -0300

    The home screen now is fully working

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import {
   setElementsToBePressed, restartManageUserInputs
 } from './app_modules/keyPress.js';
 import {
-  screenProxyBuilder, setScreens, toggleScreen
+  screenProxyBuilder, setScreens, toggleScreen, keySets
 } from './app_modules/screenManager.js';
 
 document.addEventListener('keypress', (event) => {
@@ -38,7 +38,7 @@ let screenProxyArgs = {
   nexButton: nexButton
 }
 let screenProxy = screenProxyBuilder(screenProxyArgs);
-screenProxy.lessonIndex = 0;
+screenProxy.lessonIndex = 1;
 
 previousButton.addEventListener('click', () => {
   restartManageUserInputs();
@@ -51,4 +51,77 @@ nexButton.addEventListener('click', () => {
 });
 
 setElementsToBePressed('keys');
-startManageUserInputs();
+
+let paginationLinks = document.getElementsByClassName('pagination-link');
+
+function removeIsCurrentClassFromOther() {
+  for (let link of paginationLinks) {
+    if (link.classList.contains('is-current')) {
+      link.classList.remove('is-current');
+    }
+  }
+}
+
+function addIsCurrentClass(element) {
+  element.classList.add('is-current');
+}
+
+let currentLesson = parseInt(document.querySelector('.is-current').innerHTML);
+function setCurrentLesson() {
+  currentLesson = parseInt(document.querySelector('.is-current').innerHTML);
+}
+
+function setLessonNameOnScreen() {
+  let element = document.querySelector('.content > p');
+  element.innerHTML = keySets[currentLesson].toUpperCase();
+}
+
+setLessonNameOnScreen();
+
+[...paginationLinks].forEach(element => {
+  element.addEventListener('click', () => {
+    removeIsCurrentClassFromOther();
+    addIsCurrentClass(event.target);
+    setCurrentLesson();
+    setLessonNameOnScreen();
+  });
+});
+
+function decreasePaginationLinks() {
+  if (parseInt(paginationLinks[0].innerHTML) >= 4) {
+    for (const link of paginationLinks) {
+      let value = parseInt(link.innerHTML);
+      link.innerHTML = value - 3;
+    }
+  }
+}
+
+let paginationPreviousButton = document.querySelector('.pagination-previous');
+paginationPreviousButton.addEventListener('click', () => {
+  decreasePaginationLinks();
+  setCurrentLesson();
+  setLessonNameOnScreen();
+});
+
+function increasePaginationLinks() {
+  if (parseInt(paginationLinks[2].innerHTML) <= 9) {
+    for (let link of paginationLinks) {
+      let value = parseInt(link.innerHTML);
+      link.innerHTML = value + 3;
+    }
+  }
+}
+
+let paginationNextButton = document.querySelector('.pagination-next');
+paginationNextButton.addEventListener('click', () => {
+  increasePaginationLinks();
+  setCurrentLesson();
+  setLessonNameOnScreen();
+});
+
+let startButton = document.querySelector('button.button');
+startButton.addEventListener('click', () => {
+  toggleScreen();
+  screenProxy.lessonIndex = currentLesson;
+  startManageUserInputs();
+});
